5bc7debb0b7b762d6645fd5c51211131462e9bfb
craigerskine <craig.erskine@macmillan.com>
2025-09-11 15:18:36 -0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Dashboard } from './components/Dashboard';
 import { StudySession } from './components/StudySession';
 import { Progress } from './components/Progress';
 import { StudyPlanner } from './components/StudyPlanner';
-import { Onboarding } from './components/Onboarding.1';
+import { Onboarding } from './components/Onboarding';
 
 function App() {
   const { user, currentPage, isAuthenticated } = useStore();
