4d6b8bf1aa8450fd9d1967aa4514ca51ec21ad1e
null <null>
2025-09-15 09:07:04 -0500

    figtree font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['"Figtree"', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         blue: {
           50: '#F6F0FC',
